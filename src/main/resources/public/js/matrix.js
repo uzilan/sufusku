976bbde0fe54c5f4f1267f1e6ca313ea8ca22f6c
Uzi Landsmann <uzi.landsmann@gmail.com>
2015-07-05 10:10:12 +0200

    added a log

--- src/main/resources/public/js/matrix.js
+++ src/main/resources/public/js/matrix.js
@@ -18,7 +18,7 @@ function getClass(i, j) {
 function update(data) {
     $.each(data, function (i, r) {
         $.each(r, function (j, c) {
-            var td = $('tbody')[0].children[i].children[j];
+            var td = $('tbody')[0].children[i + 1].children[j + 1];
             var input = td.children[0];
             input.value = c.value == 0 ? '' : c.value;
             var numbers = td.children[1];
@@ -31,11 +31,12 @@ function update(data) {
             numbers.style.backgroundColor = color;
         });
     });
+
+
 }
 
 function change(i, j) {
-    console.log('i: ' + i + ", j: " + j);
-    var cell = $('tbody')[0].children[i].children[j].children[0];
+    var cell = $('tbody')[0].children[i + 1].children[j + 1].children[0];
     var value = cell.value;
     var data = {'row': i, 'col': j, 'value': value};
 
@@ -48,6 +49,42 @@ function change(i, j) {
             update(result);
         }
     });
+
+    $('#log').children('a').each(function() {
+        this.className = "list-group-item";
+    });
+
+    var logRow = $('<a/>', {
+        class: "list-group-item active",
+        href: "#",
+        html: "(" + getLetter(j) + "," + (i + 1) + ") : " + value
+    });
+    $('#log').append(logRow);
+
+    $('#log_holder').scrollTop($('#log_holder')[0].scrollHeight);
+}
+
+function getLetter(i) {
+    switch (i) {
+        case 0:
+            return 'a';
+        case 1:
+            return 'b';
+        case 2:
+            return 'c';
+        case 3:
+            return 'd';
+        case 4:
+            return 'e';
+        case 5:
+            return 'f';
+        case 6:
+            return 'g';
+        case 7:
+            return 'h';
+        case 8:
+            return 'i';
+    }
 }
 
 $(document).ready(function () {
@@ -59,9 +96,27 @@ $(document).ready(function () {
         url: 'matrix',
         dataType: 'json',
         success: function (data) {
+
+            $('table').append('<tr class="axis"/>');
+            $.each([' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'], function (x, l) {
+                var letterTd = $('<td/>', {
+                    class: 'axis-top',
+                    html: l
+                });
+
+                $("table tr:last").append(letterTd);
+            });
+
             $.each(data, function (i, r) {
 
                 $('table').append('<tr/>');
+
+                var numberTd = $('<td/>', {
+                    class: 'axis-left',
+                    html: i + 1
+                });
+
+                $("table tr:last").append(numberTd);
 
                 $.each(r, function (j, c) {
 
@@ -72,7 +127,7 @@ $(document).ready(function () {
                     });
 
                     var input = $('<input/>', {
-                        type: 'text',
+                        type: 'number',
                         class: 'input',
                         html: c.value == 0 ? '' : c.value
                     }).change(function () {
